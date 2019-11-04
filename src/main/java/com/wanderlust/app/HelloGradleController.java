068f0e6ca83c3be76378f6aa6c07e5cdfd0ea8df
Xiao Lu <xiaolu@Xiaos-MacBook-Pro.local>
2019-11-04 15:05:58 -0800

    first commit

---
+++ src/main/java/com/wanderlust/app/HelloGradleController.java
@@ -0,0 +1,13 @@
+package com.wanderlust.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloGradleController {
+
+    @GetMapping
+    public String helloGradle() {
+        return "Hello Gradle!";
+    }
+}
